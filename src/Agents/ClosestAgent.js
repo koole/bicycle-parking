58fe7bd19f2ebbdd73dc9cec4eecab364e2c5b21
Leon Koole <leon@koole.io>
2022-10-22 18:33:25 +0200

    Formatting

--- src/Agents/ClosestAgent.js
+++ src/Agents/ClosestAgent.js
@@ -4,7 +4,7 @@ class ClosestAgent extends Agent {
   constructor(world, type, cell) {
     super(world, type, cell, "CLOSEST");
   }
-  
+
   // BFS for the closest valid parking spot
   find_closest_tile(valid_goal) {
     let coords = {};
@@ -31,13 +31,7 @@ class ClosestAgent extends Agent {
       ];
       for (const [y, x] of dirs) {
         let next = this.world.state[y]?.[x];
-        let valid_types = [
-          "SPAWN",
-          "BIKE_PATH",
-          "ALL_PATH",
-          "PARKING",
-          "EXIT",
-        ];
+        let valid_types = ["SPAWN", "BIKE_PATH", "ALL_PATH", "PARKING", "EXIT"];
         if (
           next !== undefined &&
           valid_types.includes(next.type) &&
@@ -48,34 +42,35 @@ class ClosestAgent extends Agent {
         }
       }
     }
-	  // no valid goal was found, just return current cell
-	  return "NO_CELL_FOUND";
+    // no valid goal was found, just return current cell
+    return "NO_CELL_FOUND";
   }
-  
+
   act() {
     this.startAct();
 
     switch (this.stage) {
       case "SPAWN":
-	      this.visited = [{x: this.cell.x, y: this.cell.y}];
-	    case "FIND_LOT":
+        this.visited = [{ x: this.cell.x, y: this.cell.y }];
+      case "FIND_LOT":
         // find lot to move to
         const new_coords = this.find_closest_tile((agent, cell) => {
-		  	const not_visited = agent.visited.every((v_cell) => 
-		  		Math.abs(v_cell.x - cell.x) + Math.abs(v_cell.y - cell.y) > 5
-		  	);
-		  	return cell.type === "PARKING" && not_visited;
-		  });
-		
-      if (new_coords === "NO_CELL_FOUND") {
-        // if we come here, we've probably already visited every lot, so we try searching all over again
-        this.stage = "SPAWN";
-      } else {
-            this.changeMoveTo(new_coords.x, new_coords.y, () => {
-        this.visited.push(new_coords);
-              this.stage = "MOVING_TO_LOT";
-            });
-      }
+          const not_visited = agent.visited.every(
+            (v_cell) =>
+              Math.abs(v_cell.x - cell.x) + Math.abs(v_cell.y - cell.y) > 5
+          );
+          return cell.type === "PARKING" && not_visited;
+        });
+
+        if (new_coords === "NO_CELL_FOUND") {
+          // if we come here, we've probably already visited every lot, so we try searching all over again
+          this.stage = "SPAWN";
+        } else {
+          this.changeMoveTo(new_coords.x, new_coords.y, () => {
+            this.visited.push(new_coords);
+            this.stage = "MOVING_TO_LOT";
+          });
+        }
         break;
 
       case "MOVING_TO_LOT":
@@ -83,11 +78,13 @@ class ClosestAgent extends Agent {
           this.stage = "FIND_SPOT";
         });
         break;
-      
+
       case "FIND_SPOT":
         // find spot to move to
         const spot_coords = this.find_closest_tile((agent, cell) => {
-          const in_lot = Math.abs(agent.cell.x - cell.x) + Math.abs(agent.cell.y - cell.y) <= 5;
+          const in_lot =
+            Math.abs(agent.cell.x - cell.x) + Math.abs(agent.cell.y - cell.y) <=
+            5;
           return cell.canPark() && in_lot;
         });
         if (spot_coords === "NO_CELL_FOUND") {
@@ -99,14 +96,13 @@ class ClosestAgent extends Agent {
           });
         }
         break;
-      
+
       case "MOVING_TO_SPOT":
         this.executePathSequence(() => {
           this.stage = "PARKING";
         });
         break;
-      
-      
+
       case "PARKING":
         if (this.park()) {
           this.stage = "LEAVING_PARKING";
